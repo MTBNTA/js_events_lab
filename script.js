0465d4f9b8f2a735f713ea77c925945cdb193cde
Tadiwanashe Dzvoti <tadiwadzvoti@hotmail.com>
2024-03-18 15:39:19 +0000

    Added delete button to list item and added function to delete text from list item with button.

--- script.js
+++ script.js
@@ -3,7 +3,13 @@ const list = document.querySelector("#list");
 
 toDoForm.addEventListener("submit", (evt) => {
     evt.preventDefault();
+    var text = evt.target["new-todo"].value;
     const newListItem = document.createElement("li");
-    newListItem.innerText = evt.target["new-todo"].value;
+    newListItem.innerHTML = '<li>' + text + ' <button class="delete-btn" onclick="Delete(this);">Delete</button> </li>';
     list.appendChild(newListItem);
 });
+
+function Delete(currentEl){
+    const listItem = currentEl.parentElement;
+    listItem.remove();
+}
